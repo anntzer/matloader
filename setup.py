119ce73bc5ae613839ab1c1ac0429645faf53db8
Antony Lee <anntzer.lee@gmail.com>
2015-03-06 01:22:13 -0800

    Indicate numpy header path.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 from Cython.Build import cythonize
+import numpy as np
 
 
 if __name__ == '__main__':
     setup(name="matloader",
           ext_modules=cythonize("matloader/*.pyx"),
+          include_dirs=[np.get_include()],
           packages=find_packages())
